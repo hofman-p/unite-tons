afd2da97e9c6288cb767afbbf1343cdcaa1fad29
Pierre HOFMAN <pierre.hofman@gmail.com>
2021-07-25 15:27:54 +0200

    Update login page to add UI and add authenticate component for logic + UI Add: Authenticate.vue component abstraction containing self logic and UI Update: Login page for UI and basic responsive

---
+++ src/components/Authenticate.vue
@@ -0,0 +1,68 @@
+<template>
+  <q-form
+    @submit="onSubmit"
+    class="full-width"
+  >
+    <q-input
+      type="email"
+      v-model="email"
+      outlined
+      label="Email"
+      lazy-rules
+      autofocus
+    />
+    <q-input
+      :type="isPwd ? 'password' : 'text'"
+      v-model="password"
+      outlined
+      lazy-rules
+    >
+      <template v-slot:append>
+        <q-icon
+          :name="isPwd ? 'visibility_off' : 'visibility'"
+          class="cursor-pointer"
+          @click="isPwd = !isPwd"
+        />
+      </template>
+    </q-input>
+    <q-checkbox class="full-width" label="Remember me" v-model="isRemembered" />
+    <q-btn class="full-width" label="Submit" type="submit" color="primary" />
+  </q-form>
+
+</template>
+<script>
+import { defineComponent, ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+export default defineComponent({
+  name: 'Authenticate',
+  setup() {
+    const router = useRouter();
+    const isRemembered = ref(false);
+    const email = ref('');
+    const password = ref('');
+    const onSubmit = async () => {
+      try {
+        // TODO: Fake authentication (axios api calls file
+        // + vuex store dispatch login to api call files)
+        if (isRemembered.value) {
+          // TODO: add cookies option to expire
+        }
+        // TODO: Cookies management and user store management
+        router.push({
+          name: 'movies',
+        });
+      } catch (e) {
+        // TODO: Notify user (Vue3 composable ??)
+      }
+    };
+    return {
+      email,
+      password,
+      isPwd: ref(true),
+      isRemembered,
+      onSubmit,
+    };
+  },
+});
+</script>
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,13 +1,37 @@
 <template>
   <q-page class="flex flex-center">
-    Login \o/
+    <div class="wrapper row justify-center">
+      <div class="column items-center">
+        <q-avatar color="red" text-color="white" icon="lock" />
+        <h5 class="signin__typo">Sign In</h5>
+      </div>
+      <authenticate />
+      <div>
+        <span>Forgot password ?</span>
+        <span>Don't have an account ? Sign Up</span>
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
+import Authenticate from 'src/components/Authenticate.vue';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'PageLogin',
+  components: { Authenticate },
 });
 </script>
+
+<style lang="sass" scoped>
+  .signin__typo
+    margin-top: 0.8rem
+    margin-bottom: 1rem
+  .wrapper
+    max-width: 444px
+
+  @media screen and (min-width: 444px)
+    .wrapper
+      width: 100%
+</style>
