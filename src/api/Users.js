dead13110cbe02e8386dc270190687f2c5ec4825
Pierre HOFMAN <pierre.hofman@gmail.com>
2021-07-25 15:48:43 +0200

    Add hard coded fake login Update: login is now completely fake \o/

--- src/api/Users.js
+++ src/api/Users.js
@@ -2,8 +2,23 @@ import { api } from 'boot/axios';
 
 export default {
   async login(data) {
-    const user = await api.post(`${process.env.API_HOSTNAME}/auth/local`, data);
-    return user.data;
+    // const user = await api.post(`${process.env.API_HOSTNAME}/auth/local`, data);
+    // return user.data;
+
+    // Fake hard coded login \o/
+    const usersMap = new Map();
+    usersMap.set('vincent@unitetons.com', { password: '12345', role: 'admin' });
+    usersMap.set('viewer@test.com', { password: '67890', role: 'viewer' });
+    const user = usersMap.get(data.email);
+
+    if (user && user.password === data.password) {
+      return {
+        user_role: user.role,
+        user_token: `${user.role}-token`,
+        user_email: data.email,
+      };
+    }
+    throw new Error('Incorrect email / password');
   },
   async register(data) {
     const user = await api.post(`${process.env.API_HOSTNAME}/auth/local/register`, data);
